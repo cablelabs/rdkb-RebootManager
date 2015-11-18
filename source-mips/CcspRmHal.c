cbdfd7251c887210863ad0b15926638a4c5ca083
Abdul Shukur <Abdul_Shukur@cable.comcast.com>
2015-11-18 13:39:15 -0500

    XF3-14: reboot-manager.bb build failure
    Reason for change: source-mips/CcspRmHal.c created
    
    Test Procedure: rebuild and verify
    
    Risks: None.
    
    Change-Id: I9b75a2195ad034c193d0509bd68595c7531bc1b8
    Signed-off-by: Abdul_Shukur@cable.comcast.com

---
+++ source-mips/CcspRmHal.c
@@ -0,0 +1,93 @@
+/*
+ * If not stated otherwise in this file or this component's Licenses.txt file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2015 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+/**********************************************************************
+   Copyright [2014] [Cisco Systems, Inc.]
+ 
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+ 
+       http://www.apache.org/licenses/LICENSE-2.0
+ 
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+**********************************************************************/
+
+/**************************************************************************
+
+    module:
+
+        CcspRmHal.c
+
+    -------------------------------------------------------------------
+
+    copyright:
+
+        Cisco Systems, Inc.
+        All Rights Reserved.
+
+    -------------------------------------------------------------------
+
+    description:
+
+        stub out for rdk-b
+
+    -------------------------------------------------------------------
+
+    author: WSN
+
+
+    -------------------------------------------------------------------
+
+    revision:
+
+        07/07/2014    initial revision.
+
+**************************************************************************/
+
+#include "ansc_platform.h"
+#include "rm_util.h"
+#include "cosa_apis_reboot.h"
+#include "cosa_apis_configuration.h"
+
+int CcspRmHal_PostRebootProcedures()
+{
+	CcspTraceDebug(("Post Reboot procedures are called here ....\n"));
+	/*
+	 * TO BE FILLED IN BY PRODUCT INSTATIATION: 
+	 
+	   Requirement CCSP_RM_5 (Post Reboot procedures)
+	   The reboot Manager provides a framework for handling any post reboot 
+	   procedures required by specific product instantiations. The actual 
+	   post reboot procedures are provided by the product instantiation. 
+	   The reboot manager calls into a component specific HAL layer after 
+	   a successful reboot indicated by the “system ready” signal delivered 
+	   by Component Registrar.  
+	 *
+     *
+    system("reboot");
+	 */
+	return 0;
+}
+
+
